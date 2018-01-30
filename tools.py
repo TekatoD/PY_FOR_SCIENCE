904f88868be94b1c447efa9bf781f790a8281e03
Arseniy Ivin <arssivka@yandex.ru>
2018-01-30 16:34:52 +0300

    database loader

--- tools.py
+++ tools.py
@@ -4,16 +4,18 @@ import array
 import numpy as np
 import cv2
 
+
 def exr_scaler_grayer(file, x, y):
     img = OpenEXR.InputFile(file)
     FLOAT = Imath.PixelType(Imath.PixelType.FLOAT)
     dw = img.header()['dataWindow']
     sz = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
-    (R,G,B) = [array.array('f', img.channel(Chan, FLOAT)).tolist() for Chan in ("R", "G", "B")]
-    np_img = np.array([(r +g + b) / 3 for r, g, b in zip(R, G, B)])
+    (R, G, B) = [array.array('f', img.channel(Chan, FLOAT)).tolist() for Chan in ("R", "G", "B")]
+    np_img = np.array([(r + g + b) / 3 for r, g, b in zip(R, G, B)])
     np_img.resize(sz[1], sz[0])
     np_img = cv2.resize(np_img, (x, y), interpolation=cv2.INTER_CUBIC)
     return np_img
+
 
 def png_scaler_grayer(file, x, y):
     img = cv2.imread(file)
@@ -21,6 +23,14 @@ def png_scaler_grayer(file, x, y):
     img = cv2.cvtColor(img, dstCn=cv2.COLOR_BGR2GRAY)
     return img
 
-def create_database(folder = 'folder', input_name = 'Image', ouput_name='Depth', samples_number = 250):
-    for i in range(250):
-        str(i).zfill(4)
+
+def create_database(folder='folder', input_name='Image', ouput_name='Depth', samples_num=250, size=(160, 120)):
+    arrays_num = 3
+    frmt = "{path}/{file}{val:0>4}.{ext}"
+    x = [np.empty((samples_num - arrays_num + 1, *size), dtype=np.float32) for arr in range(arrays_num)]
+    y = np.empty((samples_num - arrays_num + 1, *size), dtype=np.float32)
+    for iter in range(samples_num - arrays_num + 1):
+        for arr in range(arrays_num):
+            x[arr][iter] = png_scaler_grayer(frmt.format(path=folder, file=input_name, val=(iter + arr), ext='exr'), *size)
+        y[iter] = exr_scaler_grayer(frmt.format(path=folder, file=ouput_name, val=iter + arrays_num - 1, ext='png'), *size)
+    return x, y
